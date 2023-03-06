5a6e252106c07be73292eb2b735827f72c2728c4
Baden Erb <berb@okstate.edu>
2023-03-06 17:09:17 -0600

    Initial Commit

---
+++ Team_Imagine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team_Imagine
+//
+//  Created by Krystie Weckesser on 2023-03-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
