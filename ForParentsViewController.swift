a93000709684f9ec0a673dc183ff684b4e842b1b
Baden Erb <berb@okstate.edu>
2023-03-24 17:10:12 -0500

    Added segue and fixed buttons

---
+++ ForParentsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ForParentsViewController.swift
+//  Team_Imagine
+//
+//  Created by Krystie Weckesser on 2023-03-24.
+//
+
+import UIKit
+
+class ForParentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
