b0cfabf914a69f99a5d922cfb3a71947b70a1d9d
Baden Erb <berb@okstate.edu>
2023-04-04 22:27:59 -0500

    Added spin animation when logo is pressed

--- Team_Imagine/ViewController.swift
+++ Team_Imagine/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+var check = true
+
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,8 +27,22 @@ class ViewController: UIViewController {
         self.logo.layer.add(animation, forKey: "bounceAnimation")
     }
     
+    @IBAction func tapLogo(_ sender: UITapGestureRecognizer) {
+        
+        if check{
+            check = false
+            let animation = CABasicAnimation(keyPath: "transform.rotation")
+            animation.fromValue = 0.0
+            animation.toValue = CGFloat.pi * 2.0
+            animation.duration = 0.8
+            animation.repeatCount = 1
+            logo.layer.add(animation, forKey: "rotateAnimation")
+            DispatchQueue.main.asyncAfter(deadline: .now() + animation.duration) {
+                check = true
+            }
+        }
+        
+    }
     @IBOutlet weak var logo: UIImageView!
-    
-
 }
 
