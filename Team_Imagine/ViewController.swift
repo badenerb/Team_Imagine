4a80474aa640ed171ad4f3298600afac5fa96203
Baden Erb <berb@okstate.edu>
2023-04-04 21:34:51 -0500

    Added animation to the logo

--- Team_Imagine/ViewController.swift
+++ Team_Imagine/ViewController.swift
@@ -8,13 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        bounceAnimation()
         // Do any additional setup after loading the view.
         //Test
     }
 
+    func bounceAnimation() {
+        let animation = CAKeyframeAnimation(keyPath: "transform.scale")
+        animation.values = [1.0, 1.2, 0.8, 1.3, 0.9, 1.15, 1.0]
+        animation.duration = 20
+        animation.calculationMode = .cubic
+        //animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        animation.repeatCount = .infinity
+        self.logo.layer.add(animation, forKey: "bounceAnimation")
+    }
+    
+    @IBOutlet weak var logo: UIImageView!
+    
 
 }
 
