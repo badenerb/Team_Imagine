72a692ae529b8df6ad5856dd4613652a8de61505
Baden Erb <berb@okstate.edu>
2023-04-23 16:51:47 -0600

    Fixed the segues

--- Team_Imagine/ResultViewVCController.swift
+++ Team_Imagine/ResultViewVCController.swift
@@ -12,6 +12,7 @@ class ResultVCViewController: UIViewController {
     var i = 0
     var wasChoiceCorrect = false
     
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         label.textColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
@@ -25,16 +26,16 @@ class ResultVCViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    @IBOutlet weak var label: UILabel!
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+ 
+    
 }
